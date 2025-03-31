c910b01ef2199a4f57e7c5c7f44e9e07e0551913
Mohamed ZOUARI <mohamed.zouari@discoveryintech.com>
2025-03-31 09:25:02 +0100

    MZ00

---
+++ main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  region = var.region
+}
+
+resource "aws_instance" "web" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  tags = {
+    Name = "Terraform-WebServer"
+  }
+}
